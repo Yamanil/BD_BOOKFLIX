0cd7fe680fa5efed61f1d38ff7c55f6dbbcb4d98
JulioQuispe-Chavez17 <53870854+JulioQuispe-Chavez17@users.noreply.github.com>
2019-10-22 09:53:12 -0500

    DATA DEL PROYECTO

--- README/DATA/DATAM.sql
+++ README/DATA/DATAM.sql
@@ -72,3 +72,85 @@ VALUES
        (5,4,4,'Cinco dias para matar al Papa',14,'10/2019',264,'En “Cinco días para matar al Papa” el sexo, la religión y la muerte se dan cita en Santiago de Compostela a cinco días de la llegada del Papa.','N'),
        (3,3,4,'Hijos del fuego',14,'10/2019',450,'Editorial Amarante presenta "Hijos del fuego", el culmen de la trilogía de Joaquín Castillo. ','N');
  SELECT * FROM LIBRO;
+ 
+ INSERT INTO UBIGEO
+      (DEPUBI,PROUBI,DISUBI,CELUBI)
+      VALUES
+      ("LIMA","CAÑETE","SAN VICENTE",'987654321'),
+      ("CUZCO","CAÑETE","IMPERIAL",'980435432'),
+      ("LIMA","CAÑETE","LUNAHUANA",'987965432'),
+      ("LIMA","CAÑETE","QUILMANA",'954634356'),
+      ("LIMA","CAÑETE","NUEVO IMPERIAL",'911234545'),
+      ("LIMA","CAÑETE","CALANGO",'932543434'),
+      ("LIMA","CAÑETE","SANTA CRUZ",'970654234'),
+      ("LIMA","CAÑETE","CERRO AZUL",'966753312'),
+      ("LIMA","CAÑETE","ASIA",'909987123'),
+      ("LIMA","CAÑETE","TORAS",'944576331');
+      SELECT * FROM UBIGEO;
+      INSERT INTO persona
+      (NOMPER,APEPER,DNIPER,IDUBI,TIPPER,GRADAUL,SECCAUL,SECTAUL,ESTADO)
+      VALUES
+      ("Julio","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A'),
+      ("Julio Cesar","quispe chavez",'77077399',3,'A','3','A','2','A');
+  
+      SELECT * FROM persona;
+      
+         
+INSERT INTO usuario
+         (CODPER,NOMUSU,PASSUSU)
+VALUES
+         (1,'julio','tupapina'),
+         (2,'joseph','12345678'),
+         (3,'jeferson','elmaslobo'),
+         (4,'jordy','elmaslindo'),
+         (5,'jesus','julioquispe'),
+         (6,'diego','elmasrapido'),
+         (3,'yass','persona1234'),
+         (7,'sebas','persona234'),
+         (8,'sebastian','persona345'),
+         (9,'julio','persona455'),
+         (9,'jose','peSDSDA455');
+         SELECT * FROM USUARIO;
+         INSERT INTO prestamo
+         (IDBIBLI,IDLECT,IDLIB,FSALPRES,FENTPRES,CANTFALT,ESTADO)
+VALUES
+         (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+         (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+        (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+         (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+         (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+         (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+        (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+         (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+          (1,1,1,'01/02/2019','30/02/2019',4,'A'),
+         (1,1,1,'01/02/2019','30/02/2019',4,'A');
+          SELECT * FROM PRESTAMO;
+               INSERT INTO prestamo_detalle
+         (IDPRES,CANTDET,FECHENT)
+VALUES
+         (1,'2019/10/10','A'),
+  (1,'2019/10/10','A'),
+  (1,'2019/10/10','A'),
+  (1,'2019/10/10','A'),
+   (1,'2019/10/10','A'),
+  (1,'2019/10/10','A'),
+   (1,'2019/10/10','A'),
+  (1,'2019/10/10','A'),
+  (1,'2019/10/10','A'),
+   (1,'2019/10/10','A'),
+  (1,'2019/10/10','A');
+  SELECT * FROM PRESTAMO_DETALLE;
+  
+ 
+
+      
+      
+      
